3bffe57c3f4d0b9ac54187a49bc569a1eb19d179
etubom <way2young4u@hotmail.com>
2020-07-26 16:58:55 +0100

    inline styles

--- src/App.js
+++ src/App.js
@@ -26,11 +26,22 @@ class App extends Component {
       ]})
     }
   render() {
+     const style ={
+       backgroundColor: 'white',
+       font:'inherit',
+       border: '1px solid blue',
+       padding: '8px',
+       cursor: 'pointer'
+     }
     return (
       <div className="App">
        <h1>Hi,This is a react app.</h1>
        <p>Hell yeah</p>
-       <button onClick={()=>this.toggleNameHandler('Makims')}>Toggle Name</button>
+       <button 
+       style ={style}
+       onClick={()=>this.toggleNameHandler('Makims')}>
+         Toggle Name
+         </button>
        <Person 
        name={this.state.persons[0].name} 
        age={this.state.persons[0].age}
