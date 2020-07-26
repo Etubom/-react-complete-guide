0e835bf688c277331f6ce980d1ffbbd7cc8d23f2
etubom <way2young4u@hotmail.com>
2020-07-26 20:22:35 +0100

    change our list into mapped dynamic lists

--- src/App.js
+++ src/App.js
@@ -43,22 +43,13 @@ class App extends Component {
      if(this.state.showPersons){
         persons = (
           <div>
-            <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age}
-              />
-            <Person 
-            name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              />
-            <Person 
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-              click={this.toggleNameHandler.bind(this,'Moxxy')}
-              changed={this.nameChangeHandler}
-              >
-                My hobbies include : Dancing
-              </Person>
+            {this.state.persons.map(person =>{
+              return <Person
+                      name= {person.name}
+                       age={person.age}
+                      />
+            })}
+           
          </div> 
         )
      }
